889d14dae5c0234f94fc63b0aaaba5de9726b1df
Skared123 <raymundosanchez62@gmail.com>
2024-06-12 12:34:42 -0700

    init

---
+++ app/(auth)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div className='container flex p-4 justify-between text-center h-[95%] bg-red-200'>
+      <div className='bg-gray-100 container h-full'>
+        imagen
+      </div>
+      <div className='bg-gray-300 container h-full'>
+        formato de login
+      </div>
+    </div>
+  )
+}
